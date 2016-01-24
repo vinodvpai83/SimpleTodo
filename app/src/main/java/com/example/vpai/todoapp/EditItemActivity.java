22aa626b4788417b59cda5aa860072b21cdfde4f
Vinod Pai <vpPlayStore83@gmail.com>
2016-01-23 17:22:02 -0800

    Initial Commit.

---
+++ app/src/main/java/com/example/vpai/todoapp/EditItemActivity.java
@@ -0,0 +1,35 @@
+package com.example.vpai.todoapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+
+public class EditItemActivity extends AppCompatActivity {
+
+    EditText etEditItem;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_item);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        String editItemText = getIntent().getStringExtra("editItemText");
+        etEditItem = (EditText)findViewById(R.id.etEditItem);
+        etEditItem.setText(editItemText);
+        etEditItem.setSelection(editItemText.length());
+    }
+
+    public void saveEditedItem(View view) {
+        Intent data = new Intent();
+        // Pass relevant data back as a result
+        data.putExtra("editedItemText", etEditItem.getText().toString());
+        data.putExtra("code", 200); // ints work too
+        // Activity finished ok, return the data
+        setResult(RESULT_OK, data); // set result code and bundle data for response
+        finish(); // closes the activity, pass data to parent
+    }
+}
---
+++ app/src/main/java/com/example/vpai/todoapp/MainActivity.java
@@ -0,0 +1,132 @@
+package com.example.vpai.todoapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final int REQUEST_CODE = 200;
+    private int editedItemPosition;
+    ArrayList<String> todoItems;
+    ArrayAdapter<String> aTodoAdapter;
+    ListView lvItems;
+    EditText etEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        populateArrayItems();
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        lvItems.setAdapter(aTodoAdapter);
+        etEditText = (EditText)findViewById(R.id.etEditText);
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                todoItems.remove(position);
+                aTodoAdapter.notifyDataSetChanged();
+                writeItems();
+                return true;
+            }
+        });
+
+        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(MainActivity.this, EditItemActivity.class);
+                editedItemPosition = position;
+                i.putExtra("editItemText", todoItems.get(position));
+                startActivityForResult(i, REQUEST_CODE); // brings up the second activity
+            }
+        });
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // REQUEST_CODE is defined above
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            // Extract name value from result extras
+            String editedItemText = data.getExtras().getString("editedItemText");
+            int code = data.getExtras().getInt("code", 0);
+
+            // Add the updated item to the file
+            todoItems.set(editedItemPosition, editedItemText);
+            aTodoAdapter.notifyDataSetChanged();
+            writeItems();
+        }
+    }
+
+    public void populateArrayItems() {
+        readItems();
+        aTodoAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, todoItems);
+    }
+
+    private void readItems() {
+        File filesDir = getFilesDir();
+        File file = new File(filesDir, "todo.txt");
+
+        try {
+            FileUtils.touch(file);
+            todoItems = new ArrayList<String>(FileUtils.readLines(file));
+        } catch(IOException e) {
+
+        }
+    }
+
+    private void writeItems() {
+        File filesDir = getFilesDir();
+        File file = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(file, todoItems);
+        } catch(IOException e) {
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onAddItemView(View view) {
+        aTodoAdapter.add(etEditText.getText().toString());
+        etEditText.setText("");
+        writeItems();
+    }
+}
